a63022808287f3d3e4c1bf3568fdcf487f767464
Daniel Lim <daniel.k.lim@gmail.com>
2018-04-12 23:54:10 -0400

    initial commit

---
+++ skills/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Skill(models.Model):
+	name = models.CharField(max_length=255)
+	reviewed = models.DateTimeField()
+	created = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+	name = models.CharField(max_length=255)
+	reviewed = models.DateTimeField()
+	created = models.DateTimeField(auto_now_add=True)
+
+class UserSkill(models.Model):
+	user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+	skill = models.ForeignKey('Skill', on_delete=models.CASCADE)
+	proficiency = models.PositiveSmallIntegerField()
+
+class SkillCategory(models.Model):
+	category = models.ForeignKey('Category', on_delete=models.CASCADE)
+	skill = models.ForeignKey('Skill', on_delete=models.CASCADE)
